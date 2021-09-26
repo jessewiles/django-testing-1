b07fe3d6cac74b61cbe4acfe913db2d698e70d21
Jesse Wiles <jesse.wiles@gmail.com>
2021-09-26 14:14:16 -0400

    Page 4 i think

--- snippets/views.py
+++ snippets/views.py
@@ -1,47 +1,31 @@
-from django.http import HttpResponse, JsonResponse
-from django.view.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from snippets.model import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
+from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-@csrf_exempt
-def snippet_list(request):
-    """List all of the snippets"""
-    if request.method == "GET":
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
-    """Retrieve a detail of a Snippet object"""
-    try:
-        snip = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    if request.method == "GET":
-        ser = SnippetSerializer(snip)
-        return JsonResponse(ser.data, safe=False)
 
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        ser = SnippetSerializer(snip, data=data)
-        if ser.is_valid():
-            ser.save()
-            return JsonResponse(ser.data)
-        return JsonResponse(ser.errors, status=400)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    elif request.method == "DELETE":
-        snip.delete()
-        return HttpResponse(status=204)
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -25,5 +25,6 @@ router.register(r"groups", views.GroupViewSet)
 # Additionally, we include login URLS for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include("snippets.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
